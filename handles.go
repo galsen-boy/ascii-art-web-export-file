670508bc2182fc5f8730b137ecc41e4c7fccc93a
ndiba <bandiaga42@gmail.com>
2023-06-23 01:34:48 +0000

    after modif

--- handles.go
+++ handles.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	
 	"strconv"
 	"io"
+	// "strings"
+	// "bufio"
 	"os"
 	"net/http"
 	"text/template"
@@ -24,7 +27,6 @@ func StartPage(res http.ResponseWriter, req *http.Request) {
 }
 
 // Form data after submiting
-
 func SubmitTing(res http.ResponseWriter, req *http.Request) {
 	if req.Method != "POST" {
 		error405(res)
@@ -35,78 +37,90 @@ func SubmitTing(res http.ResponseWriter, req *http.Request) {
 		error404(res)
 		return
 	}
+
 	tpl, er := template.ParseFiles("./templates/home.page.tmpl")
-	if er !=nil {
+	if er != nil {
 		error500(res)
 		return
 	}
+
 	req.ParseForm()
+
 	if req.FormValue("banner") != "standard" &&
 		req.FormValue("banner") != "thinkertoy" &&
 		req.FormValue("banner") != "shadow" {
 		error500(res)
 		return
 	}
-	
+
 	txt := req.FormValue("input")
 	ban := req.FormValue("banner")
 
-	_, err := os.ReadFile(ban + ".txt")
-	if err != nil {
-		error500(res)
-		return
-	}
 	if !isPrintable(txt) {
 		error400(res)
 		return
 	}
 
-	if err != nil {
-		error500(res)
-	}
-	res.WriteHeader(200)
-	tpl.Execute(res, ConvertStr(txt, ban))
-
-	// processus pour telecharger le texte ascii-art dans un fichier
-	
 	out := ConvertStr(txt, ban)
-	fich1 := os.WriteFile("asciiResult.txt", []byte(out), 0644)
-	if fich1 != nil {
-	error500(res)
-	}
-	fich2 := os.WriteFile("asciiResult.doc", []byte(out), 0644)
-	if fich2 != nil {
+
+	// Afficher le résultat dans le template
+	res.WriteHeader(200)
+	tpl.Execute(res, out)
+	data := os.MkdirAll("data", 0755)
+	if data != nil {
 		error500(res)
+		return
 	}
-	fich3 := os.WriteFile("asciiResult.pdf", []byte(out), 0644)
-	if fich3 != nil {
+	// Enregistrer le texte ASCII dans un fichier
+	data = os.WriteFile("./data/result.txt", []byte(out), 0644)
+	if data != nil {
 		error500(res)
+		return
+	}
+	data = os.WriteFile("./data/result.exe", []byte(out), 0644)
+	if data != nil {
+		error500(res)
+		return
+	}
+	data = os.WriteFile("./data/result.pdf", []byte(out), 0644)
+	if data != nil {
+		error500(res)
+		return
 	}
 }
 
-	
-	func download(res http.ResponseWriter, req *http.Request) {
+func download(res http.ResponseWriter, req *http.Request) {
+	formatType := req.FormValue("fileformat")
+	// if formatType == "" {
+	// 	error500(res)
+	// 	return
+	// }
 
-		formatType := req.FormValue("fileformat")
-	
-		f, err := os.Open("asciiResult." + formatType)
-		if err !=nil{
-			error500(res)
-			return
-		}
+	filename := "./data/result." + formatType
 
-		defer f.Close()
-		file, _ := f.Stat()
-		fsize := file.Size()
-		sfSize := strconv.Itoa(int(fsize))
-		res.Header().Set("Content-Disposition", "attachment; filename=asciiresult."+formatType)
-		res.Header().Set("Content-Type", "text/html")
-		res.Header().Set("Content-Length", sfSize)
-		io.Copy(res, f)
-		
+	// Ouvrir le fichier à télécharger
+	f, err := os.Open(filename)
+	if err != nil {
+		error500(res)
+		return
 	}
+	defer f.Close()
 
+	// Récupérer les informations sur le fichier
+	fileInfo, err := f.Stat()
+	if err != nil {
+		error500(res)
+		return
+	}
+	fileSize := fileInfo.Size()
 
+	// Définition des en-têtes
+	res.Header().Set("Content-Disposition", "attachment; filename=result."+formatType)
+	res.Header().Set("Content-Type", "text/plain")
+	res.Header().Set("Content-Length", strconv.FormatInt(fileSize, 10))
+	f.Seek(0, io.SeekStart)
+	// Transmission du contenu du fichier
+	io.Copy(res, f)
 
+}
 
-	
